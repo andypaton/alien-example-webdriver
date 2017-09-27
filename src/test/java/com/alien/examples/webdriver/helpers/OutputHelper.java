63e2b0daacaa91460c204648dd65cde8644cb83d
Paton, Andrew <andrew.paton@sky.uk>
2017-09-27 15:21:20 +0100

    more hacking with cucumber spring

--- src/test/java/com/alien/examples/webdriver/helpers/OutputHelper.java
+++ src/test/java/com/alien/examples/webdriver/helpers/OutputHelper.java
@@ -2,14 +2,20 @@ package com.alien.examples.webdriver.helpers;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
 import com.alien.utils.report.CucumberReport.ReportBuilder;
 
-import cucumber.api.Scenario;
+import cucumber.runtime.ScenarioImpl;
 
 
+@ContextConfiguration(classes={cucumber.runtime.ScenarioImpl.class})
 public class OutputHelper {
+	
+	@Autowired ScenarioImpl scenario;
 
-    public void showMessage(Scenario scenario, String header, String message) throws IOException {
+    public void showMessage(String header, String message) throws IOException {
         new ReportBuilder().createSection(header, message).build(scenario).write();
     }
     
--- src/test/java/com/alien/examples/webdriver/steps/BaseStep.java
+++ src/test/java/com/alien/examples/webdriver/steps/BaseStep.java
@@ -7,13 +7,14 @@ import com.alien.examples.webdriver.config.CucumberConfig;
 import com.alien.examples.webdriver.helpers.OutputHelper;
 import com.alien.utils.webdriver.WebDriverUtility;
 
-@ContextConfiguration(classes={CucumberConfig.class, cucumber.api.Scenario.class})
+import cucumber.runtime.ScenarioImpl;
+
+@ContextConfiguration(classes={CucumberConfig.class, cucumber.runtime.ScenarioImpl.class})
 public class BaseStep {
 	
-//	@Autowired protected CucumberReportHelper reportHelper;
 	@Autowired protected OutputHelper outputHelper;
 	
 	protected WebDriverUtility webDriverUtility;
-//	@Autowired Scenario scenario;
+	@Autowired ScenarioImpl scenario;
 
 }
