4fc30b599e23a8a7eedb7fa1ecde78a8cedcacfb
Paton, Andrew <andrew.paton@sky.uk>
2017-10-02 08:33:55 +0100

    example xpaths added

--- src/test/java/com/alien/examples/webdriver/pageObjects/bbc/BbcHomePage.java
+++ src/test/java/com/alien/examples/webdriver/pageObjects/bbc/BbcHomePage.java
@@ -18,6 +18,14 @@ public class BbcHomePage extends PageObject {
     @FindBy(id = "idcta-username")
     private WebElement signInLink;
     
+    private static final String UNUSED_EXAMPLE_XPATH_1 ="//*[@type='radio' and @checked='checked']";
+    private static final String UNUSED_EXAMPLE_XPATH_2 = "//a[text()='Staging Area']";
+    private static final String UNUSED_EXAMPLE_XPATH_3 = "//*[@id='finish' and contains(@class, 'disabledButton')]";
+    private static final String UNUSED_EXAMPLE_XPATH_4 = "//*[@class='roomMargin']/*[contains(@class, 'containerPrimary')]";
+    private static final String UNUSED_EXAMPLE_XPATH_5 = "//*[@class='roomMargin']/*[not(contains(@class, 'containerPrimary'))]";
+    private static final String UNUSED_EXAMPLE_XPATH_6 = "//*[@type='button']/*[text() = 'Cancel']";
+    private static final String UNUSED_EXAMPLE_XPATH_7 = ".//*[contains(text(),'SUCCESS')]";
+    private static final String UNUSED_EXAMPLE_XPATH_8 = "//*[@id=\"job\"]/table/tbody/tr/td[4]/a";
     
     
     public BbcHomePage(final WebDriver webDriver) {
