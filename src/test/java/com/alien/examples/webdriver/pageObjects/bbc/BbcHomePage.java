90da3b8418af685be5b6cc3e5cbb67aad54b64ee
Paton, Andrew <andrew.paton@sky.uk>
2017-10-06 09:22:55 +0100

    more locator examples added

--- src/test/java/com/alien/examples/webdriver/pageObjects/bbc/BbcHomePage.java
+++ src/test/java/com/alien/examples/webdriver/pageObjects/bbc/BbcHomePage.java
@@ -12,14 +12,20 @@ import com.alien.utils.webdriver.pageObjects.PageObject;
 
 public class BbcHomePage extends PageObject {
     
-    private static final String UNUSED_EXAMPLE_XPATH_1 ="//*[@type='radio' and @checked='checked']";
-    private static final String UNUSED_EXAMPLE_XPATH_2 = "//a[text()='Staging Area']";
-    private static final String UNUSED_EXAMPLE_XPATH_3 = "//*[@id='finish' and contains(@class, 'disabledButton')]";
-    private static final String UNUSED_EXAMPLE_XPATH_4 = "//*[@class='roomMargin']/*[contains(@class, 'containerPrimary')]";
-    private static final String UNUSED_EXAMPLE_XPATH_5 = "//*[@class='roomMargin']/*[not(contains(@class, 'containerPrimary'))]";
-    private static final String UNUSED_EXAMPLE_XPATH_6 = "//*[@type='button']/*[text() = 'Cancel']";
-    private static final String UNUSED_EXAMPLE_XPATH_7 = ".//*[contains(text(),'SUCCESS')]";
-    private static final String UNUSED_EXAMPLE_XPATH_8 = "//*[@id=\"job\"]/table/tbody/tr/td[4]/a";
+    private static final String UNUSED_EXAMPLE_XPATH_01 ="//*[@type='radio' and @checked='checked']";
+    private static final String UNUSED_EXAMPLE_XPATH_02 = "//a[text()='Staging Area']";
+    private static final String UNUSED_EXAMPLE_XPATH_03 = "//*[@id='finish' and contains(@class, 'disabledButton')]";
+    private static final String UNUSED_EXAMPLE_XPATH_04 = "//*[@class='roomMargin']/*[contains(@class, 'containerPrimary')]";
+    private static final String UNUSED_EXAMPLE_XPATH_05 = "//*[@class='roomMargin']/*[not(contains(@class, 'containerPrimary'))]";
+    private static final String UNUSED_EXAMPLE_XPATH_06 = "//*[@type='button']/*[text() = 'Cancel']";
+    private static final String UNUSED_EXAMPLE_XPATH_07 = ".//*[contains(text(),'SUCCESS')]";
+    private static final String UNUSED_EXAMPLE_XPATH_08 = "//*[@id=\"job\"]/table/tbody/tr/td[4]/a";
+    private static final String UNUSED_EXAMPLE_XPATH_09 = "//div[starts-with(@id, 'test')]";  // starts-with
+    private static final String UNUSED_EXAMPLE_XPATH_10 = "//div[contains(@id, 'test')]"; // contains
+    
+    private static final String UNUSED_EXAMPLE_CSS_1 = "div[id^=test]";  // starts-with
+    private static final String UNUSED_EXAMPLE_CSS_2 = "div[id$=test]";  // ends-with
+    private static final String UNUSED_EXAMPLE_CSS_3 = "div[id*=test]";  // contains
 
     
     @FindBy(id = "bbccookies")
